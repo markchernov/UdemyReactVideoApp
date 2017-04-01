a1298e718b7921e1a15e4d4b02a0e8322d1f0c24
markchernov <mark.chernov@gmail.com>
2017-04-01 19:16:16 -0400

    added video list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import API_KEY from '../secret';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-import API_KEY from "../secret.js"
-
-
-//Create new component. 
-const App = () => {
-    return <div>Hi!</div>;
+ 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {videos: []};
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos});  
+    });
+  }
+  render() { 
+    return (  
+    <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+    </div>
+    );
+  } 
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
